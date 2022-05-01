bc4348ff0da8d80ef5472f2774ef0cccb1fef59b
David Oluwafemi Joshua <davidoluwafemi178@gmail.com>
2022-05-01 02:48:36 +0100

    implement data load

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
 function App() {
+  const [loading, setLoading] = useState(true);
   const [count, setCount] = useState(0)
 
+  useEffect(() => {
+    aFunc().then(() => setLoading(false))
+  })
+
+  function aFunc(){
+    return new Promise((resolve) => setTimeout(() => resolve(), 2500));
+  }
+
+  if(loading)
+    return <h1>You you yaafoiafu </h1>
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
+          <button type="button" onClick={() => setCount((count) => count + 1)} className='text-black text-5xl'>
             count is: {count}
           </button>
         </p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{jsx,tsx,js}",
+    "./src/**/*.{vue,jsx,tsx,js}",
   ],
   theme: {
     extend: {},
