7262de358c5feb2434dd8ddfd666daec7adde525
David Oluwafemi Joshua <davidoluwafemi178@gmail.com>
2022-05-09 21:59:41 +0100

    finished home page structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,jsx,tsx,js}",
-  ],
+  content: ["./index.html", "./src/**/*.{vue,jsx,tsx,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "main-bg": "url(src/bg1.jpg)",
+      },
+    },
+    fontFamily: {
+      sans: ['"Alegreya Sans SC"', "Helvetica", "sans-serif"],
+    },
   },
   plugins: [],
-}
+};
